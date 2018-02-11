5f181b49ed7e0c34bbacf4838f3db64bb845cd83
Sarah Edkins <edkins.sarah@gmail.com>
2018-02-10 22:47:37 -0500

    add punching ability

--- RageCrawl/GameScene.swift
+++ RageCrawl/GameScene.swift
@@ -43,61 +43,70 @@ extension CGPoint {
     }
 }
 
+func getXRangeOfBuilding(building:Building) -> (CGFloat, CGFloat) {
+    let halfWidth = (building.size.width / 2)
+    return (building.position.x - halfWidth, building.position.x + halfWidth)
+}
 
 class GameScene: SKScene {
     
     let playerOne:Player = Player()
     let building1:Building = Building()
     let building2:Building = Building()
-//    let dPad:DirectionalPad = DirectionalPad()
-//    let buttonRight:Button = Button()
-
+    var buildings:[Building] = []
 
     override func didMove(to view: SKView) {
         // backgroundColor = SKColor.gray
-        
-        // Background image
         let background = SKSpriteNode(imageNamed: "RageCrawlBackground1")
         background.zPosition = 0
         background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
         addChild(background)
         
-        playerOne.position = CGPoint(x: size.width * 0.8, y: size.height * 0.4)
-        playerOne.zPosition = 1
-        addChild(playerOne)
-        
-        building1.position = CGPoint(x: size.width * 0.5, y: size.height * 0.4)
+        // Create Buildings
+        building1.position = CGPoint(x: size.width * 0.5, y: size.height * 0.45)
         building1.zPosition = 1
         addChild(building1)
+        buildings.append(building1)
         
-        building2.position = CGPoint(x: size.width * 0.3, y: size.height * 0.4)
+        building2.position = CGPoint(x: size.width * 0.3, y: size.height * 0.45)
         building2.zPosition = 1
         addChild(building2)
+        buildings.append(building2)
         
-//        dPad.position = CGPoint(x: size.width * 0.1, y: size.height * 0.1)
-//        addChild(dPad)
-        
-//        buttonRight.position = CGPoint(x: size.width * 0.9, y: size.height * 0.1)
-//        addChild(buttonRight)
+        // Create Player
+        playerOne.position = CGPoint(x: size.width * 0.8, y: size.height * 0.4)
+        playerOne.zPosition = 2
+        addChild(playerOne)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        let touchCount = touches.count
         guard let touch = touches.first else {
             return
         }
-        var touchLocation = touch.location(in: self)
-        playerOne.removeAllActions()
+        let tapCount = touch.tapCount
         
-        var multiplierForDirection : CGFloat
-        if (touchLocation.x <= playerOne.position.x) {  // Note 'self.frame.midX' is middle of screen
-            multiplierForDirection = -1.0
-        } else {
-            multiplierForDirection = 1.0
+        print("touches", touchCount)
+        print("taps", tapCount)
+        
+        switch tapCount {
+            case 1:
+                var touchLocation = touch.location(in: self)
+                playerOne.removeAllActions()
+                
+                var multiplierForDirection : CGFloat
+                if (touchLocation.x <= playerOne.position.x) {  // Note 'self.frame.midX' is middle of screen
+                    multiplierForDirection = -1.0
+                } else {
+                    multiplierForDirection = 1.0
+                }
+                touchLocation.y = size.height * 0.4 // Fixed plane
+                playerOne.xScale = fabs(playerOne.xScale) * multiplierForDirection
+                playerOne.walkToDest(dest: touchLocation)
+            default:
+                playerOne.punch()
         }
-        touchLocation.y = size.height * 0.4 // Fixed plane
-        playerOne.xScale = fabs(playerOne.xScale) * multiplierForDirection
-        playerOne.walkToDest(dest: touchLocation)
         
+
     }
 }
--- RageCrawl/Player.swift
+++ RageCrawl/Player.swift
@@ -14,22 +14,28 @@ class Player: SKSpriteNode {
     
     let moveSpeed = 1.0
     let walkSpeed = 0.18
+    let punchSpeed = 0.08
     
     init() {
         let texture = SKTexture(imageNamed: "Lizzie1.png")
         super.init(texture: texture, color: .clear, size: texture.size())
     }
     
-    
     func walkToDest(dest: CGPoint) {
         let textureAtlas = SKTextureAtlas(named: "Lizzie")
         let frames = ["Lizzie1", "Lizzie2",].map { textureAtlas.textureNamed($0) }
         let animate = SKAction.animate(with: frames, timePerFrame: walkSpeed)
         let walk = SKAction.repeat(animate, count: Int(moveSpeed / walkSpeed) - 2)
-
         let move = SKAction.move(to: dest, duration: moveSpeed)
         let group = SKAction.group([walk, move])
         self.run(group)
+    }
+    
+    func punch() {
+        let textureAtlas = SKTextureAtlas(named: "LizziePunch")
+        let frames = ["liz_punch1", "liz_punch3", "liz_punch3", "liz_punch3", "liz_punch4", "liz_punch4"].map { textureAtlas.textureNamed($0) }
+        let animate = SKAction.animate(with: frames, timePerFrame: punchSpeed)
+        self.run(animate)
     }
     
     required init?(coder aDecoder: NSCoder) {
